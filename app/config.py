7258e6eb08b5c09e134aadf6759b187d07bc0886
someonecoding <54615836+someonecoding@users.noreply.github.com>
2022-09-08 04:40:52 +0300

    added config.py

---
+++ app/config.py
@@ -0,0 +1,14 @@
+from os import getenv
+
+
+class Settings:
+    # Database credentials
+    DATABASE_NAME = getenv('POSTGRES_DB')
+    DATABASE_USER = getenv('POSTGRES_USER')
+    DATABASE_PASSWORD = getenv('POSTGRES_PASSWORD')
+    DATABASE_URL = f'postgresql+asyncpg://{DATABASE_USER}:{DATABASE_PASSWORD}@database:5432/{DATABASE_NAME}'
+
+    SECRET_KEY = getenv('SECRET_KEY')
+
+
+settings = Settings()
