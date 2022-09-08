403732957e73c1cf253b835ca258a5dbb878541b
someonecoding <54615836+someonecoding@users.noreply.github.com>
2022-09-08 06:26:35 +0300

    added Apartment to alembic metadata scope

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -12,6 +12,7 @@ from config import settings
 from db import Base
 
 # Add your models here for Base metadata scope
+from models import Apartment
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
